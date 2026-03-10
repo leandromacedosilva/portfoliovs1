9ed8f89fb2cf80c885d703a0b33f657ac8ed015f
leandromacedosilva <iamfulldeveloper@gmail.com>
2026-03-10 11:36:55 -0300

    modified file index.html and added file scripts.js on project

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+function enviarWhats(event) {
+
+    event.preventDefault();
+
+    const nome = window.document.getElementById('nome').value;
+    const mensagem = window.document.getElementById('mensagem').value;
+    const telefone = '5594991790035';
+
+    const texto = `Olá, chamo-me ${nome}, ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
+
+    const URL = `https://wa.me/${telefone}/?t=${msgFormatada}`;
+
+    //console.log(URL);
+
+    window.open(URL, '_blank');
+}
